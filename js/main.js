2a94d4965bccac8f6becf5f1e03db7083fa53267
agustinmasso <agustinmasso@hotmail.com>
2025-01-15 23:50:49 -0300

    Añadiendo transición y tienda.html

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// selecciona el boton de Ingresar y la pantalla de carga
+const ingresarBtn = document.getElementById('ingresar-btn');
+const loadingScreen = document.getElementById('loading-screen');
+
+ingresarBtn.addEventListener('click', function () {
+  // Muestra la pantalla de carga
+  loadingScreen.style.visibility = 'visible';
+  loadingScreen.style.opacity = '1';
+
+  // Redirige a `tienda.html` después de la animación
+  setTimeout(() => {
+    window.location.href = 'tienda.html';
+  }, 5300); // 5,3 segundos de espera 
+});
